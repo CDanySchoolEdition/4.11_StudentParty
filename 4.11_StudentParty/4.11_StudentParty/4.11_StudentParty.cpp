4784162b7d03a8230eac1f1c06503b6584fe23aa
Daniel <constantin.eduard.e8t@student.ucv.ro>
2021-10-22 09:23:10 +0300

    4.11.1 Define party location

--- 4.11_StudentParty/4.11_StudentParty/4.11_StudentParty.cpp
+++ 4.11_StudentParty/4.11_StudentParty/4.11_StudentParty.cpp
@@ -45,20 +45,53 @@ public:
 
 class Party
 {
-  
+private:
+	Location* location;
+
+public:
+	Party(Location* location)
+	{
+		this->location = location;
+	}
+
+	void setLocation(Location* location)
+	{
+		this->location = location;
+	}
+
+	Location* getLocation()
+	{
+		return this->location;
+	}
+
+
 };
 
 int main()
 {
-	Party* party;
-	party = new Party();
 	Location* location;
 	location = new Location();
 	Location* location1 = new Location("Mircesti", 11);
-	location1->test();
+	//location1->test();
 
-	cout << endl;
+	//cout << endl;
 	location->setStreet("Spania");
 	location->setStreetNumber(423);
-	location->test();
+	//location->test();
+
+	Party* party;
+	party = new Party(location1);
+	party->getLocation()->test();
+
+	cout << endl;
+	party->setLocation(location);
+	party->getLocation()->test();
+
+	cout << endl;
+	Location* partyLocation = party->getLocation();
+	cout << partyLocation->getStreet() << endl;
+
+	party->getLocation()->setStreetNumber(10);
+	cout << endl;
+	party->getLocation()->test();
 }
